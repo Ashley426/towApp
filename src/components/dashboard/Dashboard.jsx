8f9e633b7b25588fe4394f219c72ad7e07ad4d21
Kez <markezbjc@gmail.com>
2022-01-31 01:03:48 -0500

    Changed entire design

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,27 +1,38 @@
 import './dashboard.css';
-import Buttons from './sides';
-// import MainTabs from './mainComp';
+import React, {useState} from 'react'
+// import Search from './searchBar';
+import Pages from './myRoutes';
 
 export default function Dashboard(){
+    const {active, setActive} = useState("FirstC")
+
     return(
         <div className="dashboard">
-            <header>
-                <h1>Dashboard</h1>
-            </header>
+            {/* <Search/> */}
             <aside>
-                <Buttons title="Overview"/>
-                <Buttons title="Company Profile"/>
-                <Buttons title="Service / Pricing"/>
-                <Buttons title="Dispatching Preferences"/>
-                <Buttons title="Impound Preferences"/>
-                <Buttons title="Add Vehical"/>
-                <Buttons title="Update Gas / Milage"/>
-                <Buttons title="Reports"/>
-                <Buttons title="Settings"/>
+                <nav className="outerBox">
+                    <button onClick={() => setActive("FirstC")}>Overview</button>
+                    <button onClick={() => setActive("SecondC")}>Company Profile</button>
+                    <button onClick={() => setActive("ThirdC")}>Service / Pricing</button>
+                    <button onClick={() => setActive("FourthC")}>Dispatching Preferences</button>
+                    <button onClick={() => setActive("FifthC")}>Impound Preferences</button>
+                    <button onClick={() => setActive("SixthC")}>Add Vehical</button>
+                    <button onClick={() => setActive("SeventhC")}>Update Gas / Milage</button>
+                    <button onClick={() => setActive("EighthC")}>Reports</button>
+                    <button onClick={() => setActive("NinthC")}>Settings</button>
+                </nav>
             </aside>
             <main>
-                {/* <MainTabs/> */}
-                <div className="dashOption" id="mTop">Information</div>
+                <h1>DashBoard</h1>
+                <Pages titles="overview"/>
+                {active === "SecondC" && <Pages titles="comp"/>}
+                {active === "ThirdC" && <Pages titles="service"/>}
+                {active === "FourthC" && <Pages titles="dispatch"/>}
+                {active === "FifthC" && <Pages titles="impound"/>}
+                {active === "SixthC" && <Pages titles="vehicle"/>}
+                {active === "SeventhC" && <Pages titles="gas"/>}
+                {active === "EighthC" && <Pages titles="reports"/>}
+                {active === "NinthC" && <Pages titles="settings"/>}
             </main>
         </div>
     )
--- src/components/dashboard/sides.jsx
+++
@@ -1,9 +0,0 @@
-import './dashboard.css'
-
-export default function Buttons(props){
-    return(
-        <div className="outerBox">
-            <h3>{props.title}</h3>
-        </div>
-    )
-}
