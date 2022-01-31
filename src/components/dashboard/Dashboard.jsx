bff3a76057a1f585f45dc5ef42deaf9d1e2e075f
Kez <markezbjc@gmail.com>
2022-01-31 12:11:32 -0500

    Moved SideBar into Main

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,38 +1,25 @@
 import './dashboard.css';
-import React, {useState} from 'react'
+import React from 'react'
 // import Search from './searchBar';
-import Pages from './myRoutes';
+
 
 export default function Dashboard(){
-    const {active, setActive} = useState("FirstC")
-
     return(
         <div className="dashboard">
             {/* <Search/> */}
-            <aside>
-                <nav className="outerBox">
-                    <button onClick={() => setActive("FirstC")}>Overview</button>
-                    <button onClick={() => setActive("SecondC")}>Company Profile</button>
-                    <button onClick={() => setActive("ThirdC")}>Service / Pricing</button>
-                    <button onClick={() => setActive("FourthC")}>Dispatching Preferences</button>
-                    <button onClick={() => setActive("FifthC")}>Impound Preferences</button>
-                    <button onClick={() => setActive("SixthC")}>Add Vehical</button>
-                    <button onClick={() => setActive("SeventhC")}>Update Gas / Milage</button>
-                    <button onClick={() => setActive("EighthC")}>Reports</button>
-                    <button onClick={() => setActive("NinthC")}>Settings</button>
-                </nav>
-            </aside>
             <main>
                 <h1>DashBoard</h1>
-                <Pages titles="overview"/>
-                {active === "SecondC" && <Pages titles="comp"/>}
-                {active === "ThirdC" && <Pages titles="service"/>}
-                {active === "FourthC" && <Pages titles="dispatch"/>}
-                {active === "FifthC" && <Pages titles="impound"/>}
-                {active === "SixthC" && <Pages titles="vehicle"/>}
-                {active === "SeventhC" && <Pages titles="gas"/>}
-                {active === "EighthC" && <Pages titles="reports"/>}
-                {active === "NinthC" && <Pages titles="settings"/>}
+                <nav className="outerBox">
+                    <button>Overview</button>
+                    <button>Company Profile</button>
+                    <button>Service / Pricing</button>
+                    <button>Dispatching Preferences</button>
+                    <button>Impound Preferences</button>
+                    <button>Add Vehical</button>
+                    <button>Update Gas / Milage</button>
+                    <button>Reports</button>
+                    <button>Settings</button>
+                </nav>
             </main>
         </div>
     )
--- src/components/dashboard/myRoutes.js
+++
@@ -1,9 +0,0 @@
-import './dashboard.css'
-
-export default function Pages(props){
-    return(
-        <div className={props.titles}>
-            {/* <h1>Dashboard</h1> */}
-        </div>
-    )
-}
